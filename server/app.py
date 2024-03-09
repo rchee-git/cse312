59f1e956dd593a137b3b39647f53e10487b7f750
Ryan <rchee@buffalo.edu>
2024-03-08 19:16:45 -0500

    added registration

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, redirect, render_template
+from flask import Flask, request, jsonify, redirect, render_template, make_response
 import bcrypt
 from pymongo import MongoClient
 
@@ -7,7 +7,9 @@ database = mongo["Recall"]
 users = database["users"]
 
 app = Flask(__name__)
+    
 
+@app.route("/Register", methods=['POST'])
 
 def check_pass(password,confirm):
     special_characters = "!@#$%^&()_-="
@@ -19,19 +21,37 @@ def check_pass(password,confirm):
     lowercase = False
     special = False
     number = False
-    
-@app.route("/", methods=['GET','POST'])
-def index():
-    render_template('')
-
-@app.route("/registration", methods=['POST'])
+    for character in password:
+        if character.isupper():
+            uppercase = True
+        if character.islower():
+            lowercase = True
+        if character.isdigit():
+            number = True
+        if character in special_characters:
+            special = True
+        if not character.isalnum():
+            return False
+    return (uppercase and lowercase and special and number)
 
 def register():
-    data = request.json
-    username = request.form.get('username')
-    password = request.form.get('password')
-    confirm = request.form.get('confirmPassword')
+    data = request.get_json
+    username = data.get('username')
+    password = data.get('password')
+    confirm = data.get('confirmPassword')
 
+    validate = check_pass(password, confirm)
+    if not validate:
+        return make_response('Error: Not Valid', 400)
 
-
+    search = users.find_one({'username': username})
+    if search == None:
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.enccode('utf-8'), salt)
+        user = {"username":username, "hash":hashed, "salt":salt}
+        users.insert_one(user)
+        return make_response("Registered",200)
+    else:
+        return make_response('Error: Not Valid', 400)
+    
 app.run(host="localhost",port=8080)
