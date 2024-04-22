47d878dc87e5d9b912fe21c099a7911d35319ddd
MathewDebug <mathewyeung123@gmail.com>
2024-04-21 23:41:20 -0400

    websocket backend

--- server/src/sockets/post.py
+++ server/src/sockets/post.py
@@ -8,15 +8,20 @@ from src.sockets import socketio
 
 @socketio.on("send_post")
 def send_message(data):
-    content_data = data.get("post", flush=True)
+    content_data = data.get("content")
+    print("TESTING")
+    print(content_data)
 
-    print(content_data, flush=True)
-
+    post_id = posts_collection.insert_one(
+        {
+            "content": content_data,
+            "username": 1,
+            "post_like_list": [],
+        }
+    )
     # Insert the sender_data into the database
-    post_id = posts_collection.insert_one(content_data)
     
-    serialized_message = posts_collection.find_one({"_id": post_id})
-
-    print(serialized_message, flush=True)
-    # Emit the message to the receiver's room for real-time messaging
-    emit("get_post", serialized_message)
+    # serialized_message = posts_collection.find_one({"_id": post_id})
+    # print(serialized_message, flush=True)
+    # # Emit the message to the receiver's room for real-time messaging
+    # emit("get_post", serialized_message)
